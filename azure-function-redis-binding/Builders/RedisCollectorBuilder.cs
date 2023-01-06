89b6146a0813ab94e64665be361c70f364102cfe
Jason Farrell <imjason@gmail.com>
2023-01-05 22:19:42 -0500

    refactor for redis list item save

--- azure-function-redis-binding/Builders/RedisCollectorBuilder.cs
+++ azure-function-redis-binding/Builders/RedisCollectorBuilder.cs
@@ -56,27 +56,7 @@ namespace Farrellsoft.Azure.Functions.Extensions.Redis.Builders
             if (item is IRedisListItem)
             {
                 // get all list items for the key
-                var listItemsRaw = await database.ListRangeAsync(_key);
-
-                // build the raw list into objects (or strings)
-                var listItems = listItemsRaw.Select((x, index) => new
-                {
-                    Index = index,
-                    Value = JsonConvert.DeserializeObject<TValue>(x)
-                }).ToList();
-
-                // attempt to find the item based on its given id
-                var incomingValueAsListItem = (IRedisListItem)item;
-                var foundItem = listItems.FirstOrDefault(x => ((IRedisListItem)x.Value).Id == incomingValueAsListItem.Id);
-                if (foundItem != null)
-                {
-                    await database.ListSetByIndexAsync(_key, foundItem.Index, JsonConvert.SerializeObject(item));
-                }
-                else
-                {
-                    // the item was not found by its id - insert it
-                    await database.ListRightPushAsync(_key, JsonConvert.SerializeObject(item));
-                }
+                await SaveRedisListItem((IRedisListItem)item, database);
             }
             else
             {
@@ -88,6 +68,30 @@ namespace Farrellsoft.Azure.Functions.Extensions.Redis.Builders
                 {
                     await database.ListRightPushAsync(_key, item.ToString());
                 }
+            }
+        }
+
+        async Task SaveRedisListItem(IRedisListItem item, IDatabase database)
+        {
+            var listItemsRaw = await database.ListRangeAsync(_key);
+
+            // build the raw list into objects (or strings)
+            var listItems = listItemsRaw.Select((x, index) => new
+            {
+                Index = index,
+                Value = JsonConvert.DeserializeObject<TValue>(x)
+            }).ToList();
+
+            // attempt to find the item based on its given id
+            var foundItem = listItems.FirstOrDefault(x => ((IRedisListItem)x.Value).Id == item.Id);
+            if (foundItem != null)
+            {
+                await database.ListSetByIndexAsync(_key, foundItem.Index, JsonConvert.SerializeObject(item));
+            }
+            else
+            {
+                // the item was not found by its id - insert it
+                await database.ListRightPushAsync(_key, JsonConvert.SerializeObject(item));
             }
         }
 
